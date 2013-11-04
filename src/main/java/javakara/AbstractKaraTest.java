148cf16a87721a27c35463c109049ce18c668ea8
Ulli Hafner <ullrich.hafner@gmail.com>
2013-11-04 11:24:34 +0100

    Added verification method for worlds.

--- src/main/java/javakara/AbstractKaraTest.java
+++ src/main/java/javakara/AbstractKaraTest.java
@@ -1,9 +1,12 @@
 package javakara;
 
+import static org.junit.Assert.*;
 import static org.mockito.Mockito.*;
 
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+
+import javakara.JunitKaraRunner.Orientation;
 
 import org.junit.Rule;
 import org.junit.rules.Timeout;
@@ -65,4 +68,53 @@ public abstract class AbstractKaraTest {
             throw new IllegalArgumentException(exception);
         }
     }
+
+    /**
+     * Verifies that the program under test creates the expected world and Kara is set to the expected position.
+     * The world is initialized with the start values and Kara is set to 0, 0 with orientation to the right.
+     *
+     * @param start
+     *            the start world
+     * @param expected
+     *            the expected result
+     * @param expectedKaraRow
+     *            the expected row of Kara
+     * @param expectedKaraColumn
+     *            the expected column of Kara
+     */
+    protected void verifyWorld(final String[] start, final String[] expected, final int expectedKaraRow, final int expectedKaraColumn) {
+        verifyWorld(start, expectedKaraColumn, expectedKaraColumn, Orientation.RIGHT, expected, 0, 0, Orientation.RIGHT);
+    }
+
+    /**
+     * Verifies that the program under test creates the expected world and Kara is set to the expected position.
+     * The world is initialized with the start values and Kara is set to 0, 0 with orientation to the right.
+     *
+     * @param start
+     *            the start world
+     * @param startKaraRow
+     *            the start row of Kara
+     * @param startKaraColumn
+     *            the start column of Kara
+     * @param startKaraOrientation
+     *            the start orientation of Kara
+     * @param expected
+     *            the expected result
+     * @param expectedKaraRow
+     *            the expected row of Kara
+     * @param expectedKaraColumn
+     *            the expected column of Kara
+     * @param expectedKaraOrientation
+     *            the start orientation of Kara
+     */
+    // CHECKSTYLE:OFF
+    protected void verifyWorld(final String[] start, final int startKaraRow, final int startKaraColumn, final Orientation startKaraOrientation,
+            final String[] expected, final int expectedKaraRow, final int expectedKaraColumn, final Orientation expectedKaraOrientation) {
+        JunitKaraRunner karaRunner = new JunitKaraRunner(startKaraRow, startKaraColumn, startKaraOrientation, start);
+        runProgram(karaRunner);
+
+        assertEquals("Die Welten sind nicht korrekt", new JunitKaraRunner(expectedKaraRow, expectedKaraColumn, expectedKaraOrientation,
+                expected), karaRunner);
+    }
+    // CHECKSTYLE:ON
 }

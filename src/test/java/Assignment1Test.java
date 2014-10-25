f3c6df3854e5eed47f9417f531520bfbdb23a5c5
Ulli Hafner <ullrich.hafner@gmail.com>
2014-10-25 14:12:21 +0200

    Added scenarios and tests for assignments 1-4.

---
+++ src/test/java/Assignment1Test.java
@@ -0,0 +1,35 @@
+import org.junit.Test;
+
+/**
+ * Verifies that the solution for homework assignment 1 works as expected.
+ *
+ * @author Ullrich Hafner
+ */
+public class Assignment1Test extends AbstractTimeoutKaraTest {
+    /**
+     * Checks that Kara draws the acronym 'IB' into an empty world.
+     */
+    @Test
+    public void shouldDrawIBIntoEmptyWorld() {
+        verifyWorld(9, 8, createExpectedWorld());
+    }
+
+    protected String[] createExpectedWorld() {
+        return new String[] {
+                "        ",
+                " . ...  ",
+                " . .  . ",
+                " . .  . ",
+                " . ...  ",
+                " . .  . ",
+                " . .  . ",
+                " . ...  ",
+                "        ",
+        };
+    }
+
+    @Override
+    protected Kara createProgram() {
+        return new Assignment1();
+    }
+}

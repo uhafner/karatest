29557d338c26d09823555fce33d176626f8224ad
Ulli Hafner <ullrich.hafner@gmail.com>
2015-03-24 19:13:48 +0100

    Added an example assignment to master branch.

--- scenarios/assignment0/Assignment0.java
+++ scenarios/assignment0/Assignment0.java
@@ -1,19 +1,56 @@
 /**
- * Assignment0 is a subclass of Kara. Therefore, it inherits all methods of Kara:
- * <p>
- * 
- * <i>Assignment0 ist eine Unterklasse von Kara. Sie erbt damit alle Methoden der
- * Klasse Kara:</i>
- * <p>
- * 
- * Actions: move(), turnLeft(), turnRight(), putLeaf(), removeLeaf() <b>
- * Sensors: onLeaf(), treeFront(), treeLeft(), treeRight(), mushroomFront()
+ * Example for an assignment with Greenfoot Kara.
+ *
+ * @author Ullrich Hafner
  */
 public class Assignment0 extends Kara {
     /**
-     * In the 'act()' method you can write your program for Kara.<br>
-     * <i>In der Methode 'act()' koennen die Befehle fuer Kara programmiert werden</i>
+     * Draws a diamond into a square world of size 2n + 1 x 2n +1.
      */
     public void act() {
+        int width = computeWidth();
+        int half = width / 2;
+
+        for (int line = 0; line < width; line++) {
+            int limit = computeLimit(width, line);
+
+            for (int column = 0; column < width; column++) {
+                if (column >= half - limit + 1
+                        && column < half + limit) {
+                    putLeaf();
+                }
+                move();
+            }
+
+            moveDown();
+        }
+    }
+
+    private int computeLimit(final int width, final int line) {
+        if (line > width / 2) {
+            return width - line;
+        }
+        else {
+            return line + 1;
+        }
+    }
+
+    private int computeWidth() {
+        int width = 0;
+
+        putLeaf();
+        do {
+            move();
+            width++;
+        } while (!onLeaf());
+        removeLeaf();
+
+        return width;
+    }
+
+    private void moveDown() {
+        turnRight();
+        move();
+        turnLeft();
     }
 }

feef3b99d9b9f3028394f5c815cca43ef427a885
Ulli Hafner <ullrich.hafner@gmail.com>
2015-03-24 18:38:38 +0100

    Added an example assignment to master branch.

---
+++ scenarios/assignment0/Assignment0.java
@@ -0,0 +1,19 @@
+/**
+ * Assignment0 is a subclass of Kara. Therefore, it inherits all methods of Kara:
+ * <p>
+ * 
+ * <i>Assignment0 ist eine Unterklasse von Kara. Sie erbt damit alle Methoden der
+ * Klasse Kara:</i>
+ * <p>
+ * 
+ * Actions: move(), turnLeft(), turnRight(), putLeaf(), removeLeaf() <b>
+ * Sensors: onLeaf(), treeFront(), treeLeft(), treeRight(), mushroomFront()
+ */
+public class Assignment0 extends Kara {
+    /**
+     * In the 'act()' method you can write your program for Kara.<br>
+     * <i>In der Methode 'act()' koennen die Befehle fuer Kara programmiert werden</i>
+     */
+    public void act() {
+    }
+}
---
+++ src/test/java/Assignment0Test.java
@@ -0,0 +1,62 @@
+import org.junit.Test;
+
+/**
+ * Verifies that the solution for assignment 8 works as expected. I.e., the program
+ * draws diamonds in different worlds.
+ *
+ * @author Ullrich Hafner
+ */
+public class Assignment0Test extends AbstractTimeoutKaraTest {
+    /**
+     * Verifies that the diamond is correctly drawn.
+     */
+    @Test
+    public void shouldDrawDiamondIn5x5() {
+        verifyWorld(5, 5,
+                new String[] {
+                        "  .  ",
+                        " ... ",
+                        ".....",
+                        " ... ",
+                        "  .  ",
+                 });
+    }
+
+    /**
+     * Verifies that the diamond is correctly drawn.
+     */
+    @Test
+    public void shouldDrawDiamondIn3x3() {
+        verifyWorld(3, 3,
+                new String[] {
+                        " . ",
+                        "...",
+                        " . ",
+                 });
+    }
+
+    /**
+     * Verifies that the diamond is correctly drawn.
+     */
+    @Test
+    public void shouldDrawDiamondIn9x9() {
+        verifyWorld(9, 9,
+                new String[] {
+                        "    .    ",
+                        "   ...   ",
+                        "  .....  ",
+                        " ....... ",
+                        ".........",
+                        " ....... ",
+                        "  .....  ",
+                        "   ...   ",
+                        "    .    ",
+                 });
+    }
+
+    @Override
+    protected Kara createProgram() {
+        return new Assignment0();
+    }
+}
+
